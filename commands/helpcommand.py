35bafffa15bdcfb9cbb47ec14a473be89e70673d
Sher F <finleyj415@gmail.com>
2022-12-11 02:36:42 -0500

    Moved into root directory.

---
+++ commands/helpcommand.py
@@ -0,0 +1,53 @@
+from nextcord import *
+import nextcord, json
+import time
+from nextcord.ext import commands, tasks
+from nextcord.ui import Button, View, Select
+global startTime
+startTime = time.time()
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+intents.messages = True
+intents.guilds = True
+intents.members = True
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+helpGuide = json.load(open("commands/help.json"))
+
+def createHelpEmbed(pageNum=0, inline=False):
+	pageNum = (pageNum) % len(list(helpGuide))
+	pageTitle = list(helpGuide)[pageNum]
+	embed=Embed(color=nextcord.Color.green(), title=pageTitle)
+	for key, val in helpGuide[pageTitle].items():
+		embed.add_field(name="/"+key, value=val, inline=inline)
+		embed.set_footer(text=f"Page {pageNum+1} of {len(list(helpGuide))}")
+	return embed
+
+class HelpCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @nextcord.slash_command(name="help", description="Displays command help menu.")
+    async def help(self, ctx:Interaction):
+        currentPage = 0
+        async def next_callback(interaction):
+            nonlocal currentPage, sent_msg
+            currentPage += 1
+            await sent_msg.edit(embed=createHelpEmbed(pageNum=currentPage), view=myview)
+        async def previous_callback(interaction):
+            nonlocal currentPage, sent_msg
+            currentPage -= 1
+            await sent_msg.edit(embed=createHelpEmbed(pageNum=currentPage), view=myview)
+        nextButton = Button(label="→", style=ButtonStyle.green)
+        nextButton.callback = next_callback
+        previousButton = Button(label="←", style=ButtonStyle.green)
+        previousButton.callback =  previous_callback
+        myview = View(timeout=180)
+        myview.add_item(previousButton)
+        myview.add_item(nextButton)
+        sent_msg = await ctx.send(embed=createHelpEmbed(pageNum=currentPage), view=myview)
+        print("Executed command.")
+
+def setup(bot):
+    bot.add_cog(HelpCommand(bot))
---
+++ main.py
@@ -0,0 +1,26 @@
+from nextcord import *
+import nextcord, os
+from nextcord.ext import commands
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
+token = environ["BOT_TOKEN"]
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+intents.messages = True
+intents.guilds = True
+intents.members = True
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+for fn in os.listdir("./commands"):
+    if fn.endswith(".py"):
+        bot.load_extension(f"commands.{fn[:-3]}")
+for fn in os.listdir("./events"):
+    if fn.endswith(".py"):
+        bot.load_extension(f"events.{fn[:-3]}")
+for fn in os.listdir("./levelsystem"):
+    if fn.endswith(".py"):
+        bot.load_extension(f"levelsystem.{fn[:-3]}")
+
+bot.run(token)
